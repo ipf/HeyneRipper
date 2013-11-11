d4e5f85a9506794e9529edb2f3804f8956af491e
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-11-11 17:59:09 +0100

    initial import

---
+++ Classes/HeyneRipper.php
@@ -0,0 +1,65 @@
+<?php
+namespace Ipf\HeyneRipper;
+
+/**
+ * Ripping all Documents for Heyne Digital
+ */
+class HeyneRipper {
+
+	/**
+	 * @var array
+	 */
+	protected $documents = array(
+		'rom-heyne1798' => 269,
+		'berlin-ms-germ-qrt-1666' => 311,
+		'bern-mss-muel-507' => 631,
+		'weimar-hs-2056' => 492
+	);
+
+	const BASE_URL = 'http://134.76.21.92:8080/exist/rest/db/archaeo18/queries/getText.xq?mode=raw&format=xhtml&doc=###DOC###&page=###PAGE###';
+
+	/**
+	 * @var string
+	 */
+	const TARGET_SCHEME = 'docs/###DOC###/###PAGE###.html';
+
+	/**
+	 * @return void
+	 */
+	public function main() {
+		foreach ($this->documents as $documentTitle => $numberOfPages) {
+
+			$currentDocumentUrl = str_replace('###DOC###', $documentTitle, self::BASE_URL);
+			$targetDirectory = str_replace('###DOC###', $documentTitle, self::TARGET_SCHEME);
+			$this->createDirectory($targetDirectory);
+
+			for ($i = 0; $i <= $numberOfPages; $i++) {
+				$currentUrl = str_replace('###PAGE###', $i, $currentDocumentUrl);
+				$targetFile = str_replace('###PAGE###', $i, $targetDirectory);
+
+				if (file_exists($targetFile)) {
+					echo 'Dokument ' . $documentTitle . ' existiert bereits mit Seite ' . $i . "\n";
+				} else {
+					$currentContent = file_get_contents($currentUrl);
+					try {
+						$fp = @fopen($targetFile, 'w+');
+						@fwrite($fp, $currentContent);
+						@fclose($fp);
+						echo 'Dokument ' . $documentTitle . ' hinzugefuegt mit Seite ' . $i . "\n";
+					} catch (\Exception $e) {
+						echo 'Ein Fehler trat auf: ' . $e->getMessage();
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param $targetDirectory
+	 */
+	protected function createDirectory($targetDirectory) {
+		$directoryToTest = dirname($targetDirectory);
+		mkdir($directoryToTest, 0777, TRUE);
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once('Classes/HeyneRipper.php');
+
+$heyneRipper = new \Ipf\HeyneRipper\HeyneRipper();
+$heyneRipper->main();
